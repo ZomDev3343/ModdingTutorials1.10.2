c04eee10cc984f41daca7b6ac0a1202671f7c711
ZomDev3343 <contact.lefourretout@gmail.com>
2017-09-11 18:20:29 +0200

    Créer Son Mod Minecraft Episode 21 : Ajouter des effets à l'armure quand elle est équipée.

--- src/main/java/fr/zomdev/moddingtutorials/items/armors/ItemArmorMechanical.java
+++ src/main/java/fr/zomdev/moddingtutorials/items/armors/ItemArmorMechanical.java
@@ -5,8 +5,14 @@
 package fr.zomdev.moddingtutorials.items.armors;
 
 import fr.zomdev.moddingtutorials.ZomTutoMod;
+import fr.zomdev.moddingtutorials.init.ModItems;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ItemArmorMechanical extends ItemArmor
 {
@@ -20,4 +26,25 @@ public class ItemArmorMechanical extends ItemArmor
 		this.setCreativeTab(ZomTutoMod.modTab);
 	}
 
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
+	{
+
+		if( player.inventory.armorInventory.get(0) != null && player.inventory.armorInventory.get(1) != null && player.inventory.armorInventory.get(2) != null && player.inventory.armorInventory.get(3) != null )
+		{
+
+			ItemStack helmet = player.inventory.armorInventory.get(3);
+			ItemStack chestplate = player.inventory.armorInventory.get(2);
+			ItemStack leggings = player.inventory.armorInventory.get(1);
+			ItemStack boots = player.inventory.armorInventory.get(0);
+
+			if( helmet.getItem() == ModItems.mechanical_helmet && chestplate.getItem() == ModItems.mechanical_chestplate && leggings.getItem() == ModItems.mechanical_leggings && boots.getItem() == ModItems.mechanical_boots )
+			{
+
+				player.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("speed"), 5, 2));
+
+			}
+		}
+
+	}
 }
